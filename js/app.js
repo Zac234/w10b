040f171b1f2384af7ee939b59eecf9c144bf6212
Zach <zacbenincasa@gamil.com>
2022-10-29 11:35:04 -0600

    added hopefully working code

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
-let Intro=document.getElementById('Intro')
-Intro['innertext'] = "this is an introduction"
+let header=document.getElementById('Intro')
+header['innerHTML']='this has changed the title'
+
+let end=document.getElementsByClassName('End')
+end['innerHTML']='this is definitly not the first p tag'
 
 
-let best_p_tag = document.querySelector('p')
-best_p_tag['outertext'] = " this is hopefully gonna work"
+let middle=document.querySelector('p')
+middle['innerHTML']='this is the middle of the page'
+
+let center=document.querySelectorAll('p')
+center['innerHTML']='now the p tags are the same'
