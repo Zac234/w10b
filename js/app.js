b20bcb6e0b3f66a7e31c8714430b66fcc88f2112
Zach <zacbenincasa@gamil.com>
2022-10-29 09:16:02 -0600

    added in html and js as per assignment

---
+++ js/app.js
@@ -0,0 +1,6 @@
+let Intro=document.getElementById('Intro')
+Intro['innertext'] = "this is an introduction"
+
+
+let best_p_tag = document.querySelector('p')
+best_p_tag['outertext'] = " this is hopefully gonna work"
